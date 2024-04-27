10f872c205d709364975b4a71900fa01274b3ee6
Christian Iwanzik <christian.iwanzik@gmail.com>
2024-04-27 22:28:12 +0200

    Adding ktor http server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,14 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+
+    testImplementation(libs.ktor.server.test)
+    testImplementation(libs.kotlin.test)
+}
+
 kotlin {
     jvmToolchain(21)
 }
