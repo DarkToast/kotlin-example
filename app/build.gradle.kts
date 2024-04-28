c61db446699ccca8126b5214f2036bbb79cfc5fb
Christian Schmidt <christian.iwanzik@gmail.com>
2024-04-28 22:44:51 +0200

    Addint ktlint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-   alias(libs.plugins.kotlin)
-   alias(libs.plugins.serialization)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ktlint)
 }
 
 repositories {
